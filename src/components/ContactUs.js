bfb565e245c19544fcc9f932bcd68298d13f0f96
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2023-07-25 12:03:29 +0530

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -8,26 +8,71 @@ import "./Contact.css"
 
 
 function ContactUs() {
-
-
-   const [input, setInput] = useState({
-    email: "",
-    password: "",
-    password:"",
-    number:"",
-  });
-
-  
-  function handleLogin(e) {
-    e.preventDefault();
-  }
-  
+ 
   return (
+   
+<div>
+    <div style={{ display: "grid", placeItems: "center" }}>
+      <div style={{ margin: "80px", padding: "40px", width: "70%", marginTop: "80px",marginLeft:"20px", boxShadow: "-10px  10px  10px  5px grey", display: "grid", placeItems: "center" }}>
+
+        <Form action="https://formspree.io/f/xeqdqalv" method="POST" className='form' >
+          <div className='input-filed'>
+            <h5>Send you Details</h5>
+            <Form.Group as={Row} className="mb-3 " controlId="formHorizontalName" >
+              <Form.Label column sm={8} >
+                Name
+              </Form.Label>
+              <Col sm={8}>
+                <Form.Control name="name" required type="name" placeholder="Enter Name..." />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+              <Form.Label column sm={8}>
+                Email
+              </Form.Label>
+              <Col sm={8}>
+                <Form.Control name="email" required type="email" placeholder="Enter Email..." />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+              <Form.Label column sm={8}>
+                Password
+              </Form.Label>
+              <Col sm={8}>
+                <Form.Control name="password" required type="password" placeholder="Enter Password..." />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalNumber">
+              <Form.Label column sm={8}>
+                Phone
+              </Form.Label>
+              <Col sm={8}>
+                <Form.Control name="number" required type="number" placeholder=" Enter Number..." />
+              </Col>
+            </Form.Group>
+
+            <textarea
+              name="message"
+              placeholder='Type here'
+              cols="20"
+                rows="6"
+              autoComplete="off"
+              required>
+            </textarea>
 
 
+            <div className='btn'>
+              <Button type="submit" style={{ width: "60%" }}>SEND</Button>
+            </div>
+          </div>
+        </Form>
+        <br />
+      </div>
+   
+</div>
 
-
-       <div>
+       <div >
         <div className='grid'>
           <div className="grid_card">
             <i> 💌 </i>
@@ -52,49 +97,9 @@ function ContactUs() {
             <Link to="mailto:mdsamir13968@gmail.com">  Ask the community <span>-&gt;</span></Link>
           </div>
         </div>
-    
+
 
     
-<div  style={{ margin: "40px", padding: "30px ",display:"flex",alignItems:"center", justifyContent: "center", width: "50%", boxShadow: "-10px  10px  10px  5px grey", backgroundColor: "violet" }}>
-<form action="https://formspree.io/f/xeqdqalv" method="POST" onSubmit={handleLogin}  >
-<h5><mark>Submit Your Details</mark></h5>
-  <label className='label'>Name</label><br/>
-  <input  className="form-input" type="text" name= "name" value={input.name} placeholder='enter your name...' required
-    onChange={(e) => setInput({
-        ...input,
-        [e.target.name]: e.target.value  })}/><br/>
-  <label>Email</label><br/>
-  <input className="form-input" type="email" name= "email" value={input.email} placeholder='enter your email...' required
-      onChange={(e) => setInput({
-        ...input,
-        [e.target.name]: e.target.value  })}
-  /><br/>
-  <label>Password</label><br/>
-  <input className="form-input" type="password" name= "password" value={input.password} placeholder='enter your password...' required
-      onChange={(e) => setInput({
-        ...input,
-        [e.target.name]: e.target.value  })}
-  /><br/>
-  <label>Phone</label> <br/>
-  <input className="form-input" type="number" name= "number" value={input.number} placeholder='enter your number...' required
-      onChange={(e) => setInput({
-        ...input,
-        [e.target.name]: e.target.value  })}
-       
-  /> <br/>
-  <br/>
-       <textarea
-              name="message"
-              placeholder='Type here...'
-              cols="18"
-               rows="6"
-              autoComplete="off"
-              required>
-            </textarea>
-   <Button style={{marginTop:"10px",borderRadius:"10px",fontSize:"12px"}} type='submit' variant='warning'>Submit</Button>
-</form>
-</div>
-
       <iframe className='iframe' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.8598836045694!2d88.29019411443356!3d22.546920939596063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0279a4f4de010d%3A0x30e59320ebc2678a!2sDream%20Palace!5e0!3m2!1sen!2sin!4v1660218723995!5m2!1sen!2sin"
         width="100%" height="450" style={{ border: 0 }}
         allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
@@ -103,7 +108,10 @@ function ContactUs() {
     </div>
 
 
+</div>
+
   )
 }
 
 export default ContactUs
+
