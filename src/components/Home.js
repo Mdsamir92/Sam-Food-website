0561e3ca170468d52fb0439530a0098e6a7fb737
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2023-12-06 15:05:05 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,16 +30,7 @@ function Home() {
         <img  src={Img[activeImg]} alt="wallpaper" />
         <svg onClick={handleNextClick }  xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>  
     </div>
-  //   <div className="home">
-  //   <div className="headerContainer">
-  // <h1> Unlock exclusive <br/> offers🎉🎊</h1>
-  // <p>Enjoy up to 50% off  </p>
-  // <p>on your first order</p>
-  // <Link to="/menu">
-  // <button>Order now </button>
-  // </Link>
-  // </div>
-  // </div>
+
 
   <div className='row  working-time'>
     <div className='col-md-3  container '>
