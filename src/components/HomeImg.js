a3f96fdfeaf4979271bbdb6483466da5aaa7544a
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2023-12-06 14:51:56 +0530

    Add files via upload

---
+++ src/components/HomeImg.js
@@ -0,0 +1,5 @@
+export const Img  = ["https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1490818387583-1baba5e638af?q=80&w=1464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+,"https://cdn.pixabay.com/photo/2017/06/02/18/24/watermelon-2367029_1280.jpg",
+"https://cdn.pixabay.com/photo/2016/11/30/15/23/apples-1873078_1280.jpg",
+"https://img.freepik.com/free-photo/chicken-skewers-with-slices-apples-chili-top-view_2829-19996.jpg?w=740&t=st=1701843449~exp=1701844049~hmac=bb8150f9508be5009720cc40aa9d24e834871f510e8a8151cc8b7179ac6e1511",];
