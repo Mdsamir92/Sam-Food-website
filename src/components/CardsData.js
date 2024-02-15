673bac32c086a16ebae76f04fd5d32dd291d2751
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-02-15 17:27:48 +0530

    Update CardsData.js

--- src/components/CardsData.js
+++ src/components/CardsData.js
@@ -1,7 +1,7 @@
 const Cardsdata = [
     {
         id: 1,
-        rname: " Chicken Biryani",
+        dishName: " Chicken Biryani",
         imgdata: "https://www.thedeliciouscrescent.com/wp-content/uploads/2019/04/Chicken-Biryani-Square.jpg",
         address: "South Indian, Biryani, Mughlai",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -13,7 +13,7 @@ const Cardsdata = [
     },
     {
         id: 2,
-        rname: "Chicken Lolipop",
+        dishName: "Chicken Lolipop",
         imgdata: "https://jamilghar.com/wp-content/uploads/2022/01/grilled-tandoori-chicken13-scaled.jpeg",
         address: "Street Food",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -25,7 +25,7 @@ const Cardsdata = [
     },
     {
         id: 3,
-        rname: "Chicken Kabab",
+        dishName: "Chicken Kabab",
         imgdata: "https://glebekitchen.com/wp-content/uploads/2016/12/chickentikkakebab.jpg",
         address: "Kabab adda",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -37,7 +37,7 @@ const Cardsdata = [
     },
     {
         id: 4,
-        rname: "Chicken Burger",
+        dishName: "Chicken Burger",
         imgdata: "https://img.freepik.com/premium-photo/amazing-delicious-cheese-burger_727939-299.jpg",
         address: "fast foodr",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -49,7 +49,7 @@ const Cardsdata = [
     },
     {
         id: 5,
-        rname: "Pizza",
+        dishName: "Pizza",
         imgdata: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS.jpg",
         address: " fast food",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -61,7 +61,7 @@ const Cardsdata = [
     },
     {
         id: 6,
-        rname: "Chicken Rolls",
+        dishName: "Chicken Rolls",
         imgdata: "https://uploads-ssl.webflow.com/5c481361c604e53624138c2f/60f2ea67b471327a1d82959b_chicken%20roll_1500%20x%201200.jpg",
         address: "Wraps FastFood, Chinese",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -71,9 +71,47 @@ const Cardsdata = [
         arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
          qnty:0
     },
+
     {
         id: 7,
-        rname: "Cold Coffee",
+        dishName: "cake",
+        imgdata: "https://i.pinimg.com/564x/96/cc/5c/96cc5cc7be5a57e7ea9e2be477937657.jpg",
+        address: "delicious cake",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 350,
+        rating: "5.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        dishName: "chocolate cake",
+        imgdata: "https://i.pinimg.com/564x/58/41/e3/5841e315cb972bd7da483965179699cc.jpg",
+        address: "delicious cake",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 1000,
+        rating: "5.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+
+    {
+        id: 9,
+        dishName: "cake",
+        imgdata: "https://i.pinimg.com/564x/f7/4c/cf/f74ccfa113c24853c61949d1ba90a653.jpg",
+        address: "delicious cake",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 2000,
+        rating: "5.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 10,
+        dishName: "Cold Coffee",
         imgdata: "https://i0.wp.com/cookingfromheart.com/wp-content/uploads/2022/04/Cold-Coffee-3.jpg?resize=684%2C1024&ssl=1",
         address: "Coffee",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -84,8 +122,8 @@ const Cardsdata = [
          qnty:0
     },
     {
-        id: 8,
-        rname: "Sprite",
+        id: 11,
+        dishName: "Sprite",
         imgdata: "https://m.media-amazon.com/images/I/61slsTqQruL._SL1500_.jpg",
         address: "cold drink",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -96,8 +134,8 @@ const Cardsdata = [
          qnty:0
     },
     {
-        id: 9,
-        rname: "Chai",
+        id: 12,
+        dishName: "Chai",
         imgdata: "https://www.honeywhatscooking.com/wp-content/uploads/2019/04/Kadak-Masala-Chai-3.jpg",
         address: "Tea, Coffee, Shake, Beverages",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -108,8 +146,8 @@ const Cardsdata = [
          qnty:0
     },
     {
-        id: 10,
-        rname: "maggie",
+        id: 13,
+        dishName: "maggie",
         imgdata: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/07/15/chinese.jpg",
         address: "chinese food",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -120,8 +158,8 @@ const Cardsdata = [
          qnty:0
     },
     {
-        id: 11,
-        rname: "pasta",
+        id: 14,
+        dishName: "pasta",
         imgdata: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FEdit%2F09-2022-sausage-pasta%2Fsausage-pasta-4",
         address: "chinese food",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -132,8 +170,8 @@ const Cardsdata = [
          qnty:0
     },
     {
-        id: 12,
-        rname: "momos",
+        id: 15,
+        dishName: "momos",
         imgdata: "https://images.news18.com/ibnkhabar/uploads/2021/08/vegetable-momos.jpg?im=FitAndFill,width=1200,height=675",
         address: "chinese food",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
@@ -143,6 +181,7 @@ const Cardsdata = [
         arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
          qnty:0
     },
+   
     
 ];
 
