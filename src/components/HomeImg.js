3595abb2666e5728a541733cff1dc9b333f6e2f1
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-02-22 14:42:53 +0530

    Update HomeImg.js

--- src/components/HomeImg.js
+++ src/components/HomeImg.js
@@ -1,5 +1,6 @@
 export const Img  = ["https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-  " https://pixabay.com/photos/cheese-snacks-appetizer-nutrition-1887233/ ", 
+  "https://pixabay.com/photos/cheese-snacks-appetizer-nutrition-1887233/", 
+  "https://pixabay.com/photos/breakfast-healthy-hummus-spread-1804457/", 
 
 "https://img.freepik.com/free-photo/chicken-skewers-with-slices-apples-chili-top-view_2829-19996.jpg?w=740&t=st=1701843449~exp=1701844049~hmac=bb8150f9508be5009720cc40aa9d24e834871f510e8a8151cc8b7179ac6e1511",
 "https://i.pinimg.com/564x/c9/4a/ff/c94affe6bcb9fbf7d3508ba98977a1d1.jpg"];
