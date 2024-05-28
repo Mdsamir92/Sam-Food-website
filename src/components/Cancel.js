d7887226aa56f051e79870823d7c12f0cff6d70d
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-28 20:54:58 +0530

    Create Cancel.js

---
+++ src/components/Cancel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cancel() {
+  return (
+    <div style={{height:"90vh"}} className='mt-5'>
+
+        <h1 className='text-center '>Error 404</h1>
+    </div>
+  )
+}
+
+export default Cancel
